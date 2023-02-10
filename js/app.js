fd6f00e0e55a7bb5fa9539249bc94c16a89935e7
Anastasia Mitrofanova <mitrofanovaa70@gmail.com>
2023-02-11 01:59:11 +0300

    исправляет скрипт

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-ScrollSmoother.create({
-  wrapper: '.wrapper',
-  content: '.content',
-  smooth: 1.5,
-  effects: true
-})
+if(ScrollTrigger.isTouch !==1) {
+  ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5,
+    effects: true
+  })
+
+}
