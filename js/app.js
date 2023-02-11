35362a629118b86cd4a04d4231b24799e738259b
Anastasia Mitrofanova <mitrofanovaa70@gmail.com>
2023-02-11 18:31:46 +0300

    Добавляет анимацию элементам в портфолио

--- js/app.js
+++ js/app.js
@@ -8,4 +8,41 @@ if(ScrollTrigger.isTouch !==1) {
     effects: true
   })
 
-}
+  gsap.fromTo('.hero-section', { opacity: 1 }, {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.hero-section',
+      start: 'center',
+      end: '820,',
+      scrub: true
+    }
+  })
+
+  let itemsLeft = gsap.utils.toArray('.gallery__left .gallery__item')
+  itemsLeft.forEach(item => {
+    gsap.fromTo(item, { x: -100, opacity: 0 }, {
+      opacity: 1, x: 0,
+      scrollTrigger: {
+        trigger: item,
+        start: '-850',
+        end: '-100',
+        scrub: true
+      }
+    }) 
+  })
+
+  let itemsRight = gsap.utils.toArray('.gallery__right .gallery__item')
+  itemsRight.forEach(item => {
+    gsap.fromTo(item, { x: 100, opacity: 0 }, {
+      opacity: 1, x: 0,
+      scrollTrigger: {
+        trigger: item,
+        start: '-850',
+        end: '-100',
+        scrub: true
+      }
+    }) 
+  })
+ }
+
+ 
